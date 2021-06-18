0e6eeb8bc7444aab0caa06170f8964114bdb62ee
Daniel <leniadz2@gmail.com>
2021-06-17 19:02:04 -0500

    0617 2

--- Views/bds.COM_CONTRATOS.sql
+++ Views/bds.COM_CONTRATOS.sql
@@ -1,26 +1,32 @@
 ﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
 GO
-CREATE VIEW [bds].[COM_CONTRATOS] AS
-  select vn.[Clave RE]                 as VICNCN_ClaveRE
-        ,vn.[Sociedad]                 as VICNCN_sociedad
-        ,vn.[Contrato]                 as VICNCN_contrato
-        --,vn.[Nº objeto]                as VICNCN_nroObjeto
-        ,vn.[Clv.bien.inmb.]           as VICNCN_clvBienInmb
-        ,vn.[Denominación de contrato] as VICNCN_denominacionContrato
-        ,vn.[Contrato anterior]        as VICNCN_contratoAnterior
-        ,vn.[Moneda del contrato]      as VICNCN_monedaContrato
-        --,vl.[UID: IC/Objeto]           as VIBPOBJREL_UID_ICObjeto
-        --,vl.[Clave RE]                 as VIBPOBJREL_ClaveRE
-        --,vl.[Interloc.comercial]       as VIBPOBJREL_Interloccomercial
-        --,vl.[Función]                  as VIBPOBJREL_Función
-        ,vl.[Inicio relación]          as VIBPOBJREL_Iniciorelacion
-        ,vl.[Final relación]           as VIBPOBJREL_Finalrelacion
-        --,dm.[Interloc.comercial]       as DFKKBPTAXNUM_Interloccomercial
-        ,dm.[N.I.F.]                   as DFKKBPTAXNUM_NIF
-   from stg.SAPRE_VICNCN as vn 
-          left join stg.SAPRE_VIBPOBJREL as vl
-            on vn.[Clave RE] = vl.[Clave RE]
-          left join stg.SAPRE_DFKKBPTAXNUM as dm
-            on vl.[Interloc.comercial] = dm.[Interloc.comercial]
-    where vl.[Función] = 'TR0600';
+
+CREATE VIEW [bds].[COM_CONTRATOS]
+AS
+/* usa tres tablas de RE para obtener el RUC por cada contrato
+*/
+SELECT
+  vn.[Clave RE] AS VICNCN_ClaveRE
+ ,vn.[Sociedad] AS VICNCN_sociedad
+ ,vn.[Contrato] AS VICNCN_contrato
+  --,vn.[Nº objeto]                as VICNCN_nroObjeto
+ ,vn.[Clv.bien.inmb.] AS VICNCN_clvBienInmb
+ ,vn.[Denominación de contrato] AS VICNCN_denominacionContrato
+ ,vn.[Contrato anterior] AS VICNCN_contratoAnterior
+ ,vn.[Moneda del contrato] AS VICNCN_monedaContrato
+  --,vl.[UID: IC/Objeto]           as VIBPOBJREL_UID_ICObjeto
+  --,vl.[Clave RE]                 as VIBPOBJREL_ClaveRE
+  --,vl.[Interloc.comercial]       as VIBPOBJREL_Interloccomercial
+  --,vl.[Función]                  as VIBPOBJREL_Función
+ ,vl.[Inicio relación] AS VIBPOBJREL_Iniciorelacion
+ ,vl.[Final relación] AS VIBPOBJREL_Finalrelacion
+ ,ROW_NUMBER() OVER (PARTITION BY vl.[Clave RE] ORDER BY vl.[Final relación] DESC) AS VIBPOBJREL_ordInterloccomercial
+  --,dm.[Interloc.comercial]       as DFKKBPTAXNUM_Interloccomercial
+ ,dm.[N.I.F.] AS DFKKBPTAXNUM_NIF
+FROM stg.SAPRE_VICNCN AS vn
+LEFT JOIN stg.SAPRE_VIBPOBJREL AS vl
+  ON vn.[Clave RE] = vl.[Clave RE]
+LEFT JOIN stg.SAPRE_DFKKBPTAXNUM AS dm
+  ON vl.[Interloc.comercial] = dm.[Interloc.comercial]
+WHERE vl.[Función] = 'TR0600';
 GO
---
+++ Views/bds.SRVCOM_OPERADORES.sql
@@ -0,0 +1,32 @@
+﻿SET QUOTED_IDENTIFIER, ANSI_NULLS ON
+GO
+
+CREATE VIEW [bds].[SRVCOM_OPERADORES]
+AS
+/* toma los registros del SRV y les agrega data del RE como el gafo
+*/
+SELECT
+  so.*
+ ,cc.DFKKBPTAXNUM_NIF
+ ,cc.VICNCN_sociedad
+ ,cc.VICNCN_contrato
+ ,cc.VICNCN_clvBienInmb
+ ,cc.VICNCN_denominacionContrato
+ ,co.Contrato
+ ,co.InicioContrato
+ ,co.FinContrato
+ ,co.VigenciaContrato
+ ,co.NombreComercial
+ ,co.ObjetoAlquiler
+ ,co.Gafo
+ ,co.SubGafo
+FROM BD_SRV.tmp.operadoresSRV so
+LEFT JOIN bds.COM_CONTRATOS cc
+  ON so.RUCEMISOR = cc.DFKKBPTAXNUM_NIF
+    AND so.CONTRATO_RE = cc.VICNCN_contrato
+    AND IIF(so.CODIGOMALL = '01', '3001', IIF(so.CODIGOMALL = '02', '4007', IIF(so.CODIGOMALL = '03', '3001', NULL))) = cc.VICNCN_sociedad
+LEFT JOIN stg.COM_OPERADORES co
+  ON CONCAT(cc.VICNCN_sociedad, 'CC - 000', cc.VICNCN_contrato) = co.Contrato
+    AND CONCAT(SUBSTRING(so.fecha, 1, 4), SUBSTRING(so.fecha, 6, 2)) = co.Periodo
+WHERE cc.VIBPOBJREL_ordInterloccomercial = 1;
+GO
